1be9d99f1bf8a988d759e31a84b600c32751e52d
lelesaad <54856892+lelesaad@users.noreply.github.com>
2024-10-13 13:38:03 +0300

    Setup

---
+++ app.js
@@ -0,0 +1,21 @@
+
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const expressLayout = require('express-ejs-layouts');
+const PORT = 3000 || process.env.PORT;
+
+app.use(express.static('public'));
+
+// templaiting Engine
+app.use(expressLayout);
+app.set('layout','./layouts/main');
+app.set('view engine','ejs');
+
+app.use('/', require('./server/routes/main'));
+
+app.listen(PORT,()=>{
+    console.log(`App listening on port ${PORT}`);
+    
+})
---
+++ server/routes/main.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+const router = express.Router();
+
+
+
+//Routes 
+router.get('', (req, res)=>{
+    const locals = {
+        title: "NodeJs Blog",
+        description : "Simple Blog created with NoDeJs, Express& MongoDb"
+    }
+    res.render('index',{locals});
+});
+
+router.get('/about', (req, res)=>{
+    res.render('about');
+});
+
+router.get('/contact', (req, res)=>{
+    res.render('contact');
+});
+
+module.exports = router;
