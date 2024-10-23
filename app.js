8a79ea13d41e843afd8c8c2918144bf34daaea54
Faldukkan <fatimahkd97@gmail.com>
2024-10-23 20:02:13 +0300

    fatima

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
 //NouraF- note that the path is a little different it's under routes
 const connectDB = require('./server/config/db');
 
@@ -13,6 +17,20 @@ const PORT = 3000 || process.env.PORT;
 
 // Connect to DB ..
 connectDB();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method'));
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI
+    }),
+    //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+  }));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -25,6 +43,7 @@ app.set('layout','./layouts/main');
 app.set('view engine','ejs');
 
 app.use('/', require('./server/routes/main'));
+app.use('/', require('./server/routes/admin'));
 
 app.listen(PORT,()=>{
     console.log(`App listening on port ${PORT}`);
